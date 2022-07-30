853b21801f275994ec907f38fd1b791587164234
Nitesh Kumar <KumarNitesh@outlook.com>
2022-07-30 11:54:37 +0530

    Dutch Nation Problem

---
+++ Algorithm/DutchNationFlag/BruteForce.cs
@@ -0,0 +1,28 @@
+﻿namespace DutchNationFlag
+{
+    public static class BruteForce
+    {
+        public static int[] Sort(int[] array)
+        {
+            int low = 0;
+            int high = array.Length - 1;
+
+            for (int mid = 0; mid <= high; mid++)
+            {
+                if (array[mid] == 0)
+                    Swap(array, low++, mid--);
+
+                if (array[mid] == 2)
+                    Swap(array, mid--, high--);
+            }
+
+            return array;
+        }
+        private static void Swap(int[] array, int x, int y)
+        {
+            int temp = array[x];
+            array[x] = array[y];
+            array[y] = temp;
+        }
+    }
+}
---
+++ Algorithm/DutchNationFlag/Program.cs
@@ -0,0 +1,6 @@
+﻿using DutchNationFlag;
+
+//int[] array = { 2, 2, 2, 0, 0, 0, 1, 1 };
+int[] array = { 1, 2, 0 };
+var result1 = BruteForce.Sort(array);
+Console.WriteLine("[{0}]", string.Join(", ", result1));
