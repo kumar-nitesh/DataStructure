2d704db00cd792d3870a04ea4313393e81ee0a8c
Nitesh Kumar <KumarNitesh@outlook.com>
2022-10-29 14:27:03 +0530

    Recursion - Flood Fill, Special Keyboard, Josephus Problem and Number of Paths

---
+++ Algorithms/Recursion/FloodFill.cs
@@ -0,0 +1,64 @@
+﻿/********************************************************************************************
+ * Flood fill Algorithm.
+ * 
+ * Source     : Geeks for Geeks(Must Do)
+ * Difficulty : Recursion - Medium
+ * Problem    : https://practice.geeksforgeeks.org/problems/flood-fill-algorithm1856/1 
+ * Solution   : 
+ * 
+ * Time Complexity  : O(n*m)        
+ * Space complexity : O(n*m) 
+ ********************************************************************************************/
+
+namespace Algorithms.Recursion
+{
+    public class FloodFill : IExecute
+    {
+        int[,] FloodFillAlgorithm(int[,] image, int sr, int sc, int newColor)
+        {
+            if (image[sr, sc] == newColor)
+                return image;
+
+            Fill(image, sr, sc, image[sr, sc], newColor);
+
+            return image;
+        }
+
+        private void Fill(int[,] image, int sr, int sc, int color, int newColor)
+        {
+            if (sr < 0 || sr >= image.GetLength(0) || sc < 0 || sc >= image.GetLength(1)
+                || image[sr, sc] != color)
+            {
+                return;
+            }
+
+            image[sr, sc] = newColor;
+            Fill(image, sr - 1, sc, color, newColor);// North
+            Fill(image, sr + 1, sc, color, newColor);// South
+            Fill(image, sr, sc - 1, color, newColor);// West
+            Fill(image, sr, sc + 1, color, newColor);// East
+
+            return;
+        }
+
+        public void Execute()
+        {
+            int[,] image = new int[3, 3]
+            {
+                { 1, 1, 1 },
+                { 1, 1, 0 },
+                { 1, 0, 1 }
+            };
+
+            var result = FloodFillAlgorithm(image, 1, 1, 2);
+
+            for (int i = 0; i < image.GetLength(0); i++)
+            {
+                for (int j = 0; j < image.GetLength(1); j++)
+                {
+                    Console.WriteLine("image[{0},{1}] = {2}", i, j, image[i, j]);
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms/Recursion/NumberOfPaths.cs
@@ -0,0 +1,89 @@
+﻿/********************************************************************************************
+ * Number of paths.
+ * 
+ * Source     : Geeks for Geeks(Must Do)
+ * Difficulty : Recursion - Easy
+ * Problem    : https://practice.geeksforgeeks.org/problems/number-of-paths0926/1 
+ * Solution   : https://www.youtube.com/watch?v=rBAxUTqvlQA
+ *              https://www.codingninjas.com/codestudio/library/count-all-number-of-paths-of-a-given-matrix
+ * 
+ * Time Complexity  : O(m * n)        
+ * Space complexity : O(m * n)  
+ ********************************************************************************************/
+
+namespace Algorithms.Recursion
+{
+    public class NumberOfPaths : IExecute
+    {
+        /// <summary>
+        /// Recursive Dynamic Programming solution
+        /// Time Complexity  : O(mn)      
+        /// Space complexity : O(mn)
+        /// </summary>
+        long Paths(int m, int n)
+        {
+            int[,] dp = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        dp[i, j] = 1;
+                    }
+                    else
+                    {
+                        dp[i, j] = dp[i, j - 1] + dp[i - 1, j];
+                    }
+                }
+            }
+            return dp[m - 1, n - 1];
+        }
+
+        /// <summary>
+        /// Space Optimization of DP solution
+        /// Time Complexity  : O(m * n)      
+        /// Space complexity : O(n)
+        /// </summary>
+        long PathsUsingDPSpace(int m, int n)
+        {
+            int[] dp = new int[n];
+            dp[0] = 1;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    dp[j] += dp[j - 1];
+                }
+            }
+            return dp[n - 1];
+        }
+
+        /// <summary>
+        /// Using Combinatorics
+        /// Total number of paths= M+N-2 C N-1 (3C2)
+        /// Result = (m+n-2)! / (n-1)! (m-1)!
+        /// Time Complexity  : O(m + n)      
+        /// Space complexity : O(1)
+        /// </summary>
+        long PathUsingCombinatorics(int m, int n)
+        {
+            int result = 1;
+            for (int i = n; i < m + n - 1; i++)
+            {
+                result *= i;
+                result /= (i - n + 1);
+            }
+
+            return result;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Number of Paths : " + Paths(3, 3));
+            Console.WriteLine("Number of Paths : " + PathsUsingDPSpace(3, 7));
+            Console.WriteLine("Number of Paths : " + PathUsingCombinatorics(3, 2));
+        }
+    }
+}
