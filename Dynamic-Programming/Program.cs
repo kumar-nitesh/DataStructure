bccaf6b61fdb53836b3587cf375fdf854b5f0f84
Nitesh Kumar <KumarNitesh@outlook.com>
2022-11-14 17:58:56 +0530

    DP - Longest Common Substring/Subsequence

--- Dynamic-Programming/Program.cs
+++ Dynamic-Programming/Program.cs
@@ -7,7 +7,7 @@ var types = AppDomain.CurrentDomain.GetAssemblies()
 
 foreach (var t in types)
 {
-    if (t.Name == "MinimumCoins")
+    if (t.Name == "LongestCommonSubsequence")
     {
         var initiatedObject = (IExecute?)Activator.CreateInstance(t);
         initiatedObject?.Execute();
--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -7,7 +7,7 @@ var types = AppDomain.CurrentDomain.GetAssemblies()
 
 foreach (var t in types)
 {
-    if (t.Name == "MinimumElement")
+    if (t.Name == "StreamingAlgorithm")
     {
         var initiatedObject = (IExecute?)Activator.CreateInstance(t);
         initiatedObject?.Execute();
