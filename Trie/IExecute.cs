8f4f672e8a8b0a5553390b4963e1842c736e22b9
Nitesh Kumar <KumarNitesh@outlook.com>
2023-06-13 21:55:08 +0530

    Trie - Operations

---
+++ Trie/IExecute.cs
@@ -0,0 +1,7 @@
+﻿namespace Trie
+{
+    public interface IExecute
+    {
+        void Execute();
+    }
+}
---
+++ Trie/Program.cs
@@ -0,0 +1,15 @@
+﻿using Trie;
+
+var type = typeof(IExecute);
+var types = AppDomain.CurrentDomain.GetAssemblies()
+    .SelectMany(s => s.GetTypes())
+    .Where(p => type.IsAssignableFrom(p) && !p.IsInterface);
+
+foreach (var t in types)
+{
+    if (t.Name == "Trie")
+    {
+        var initiatedObject = (IExecute?)Activator.CreateInstance(t);
+        initiatedObject?.Execute();
+    }
+}
