5aabded160e4488d514b39d0fbc417e17adc9571
Nitesh Kumar <KumarNitesh@outlook.com>
2023-07-17 17:12:41 +0530

    Generic code using Reflection for running programs

--- DataStructures.Algorithms/Program.cs
+++ DataStructures.Algorithms/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataStructures.Algorithms;
+
+var type = typeof(IExecute);
+var types = AppDomain.CurrentDomain.GetAssemblies()
+    .SelectMany(s => s.GetTypes())
+    .Where(p => type.IsAssignableFrom(p) && !p.IsInterface);
+
+foreach (var t in types)
+{
+    if (t.Name == "Anagrams")
+    {
+        var initiatedObject = (IExecute?)Activator.CreateInstance(t);
+        initiatedObject?.Execute();
+    }
+}
