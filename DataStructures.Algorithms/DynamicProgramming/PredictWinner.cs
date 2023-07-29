1dd92f64233101f6920fa85d402cf8b3ac7accb8
Nitesh Kumar <KumarNitesh@outlook.com>
2023-07-29 22:57:13 +0530

    Dynamic Programming - Predict Winner and Soup Servings

---
+++ DataStructures.Algorithms/DynamicProgramming/PredictWinner.cs
@@ -0,0 +1,51 @@
+﻿/********************************************************************************************
+ * Predict the Winner.
+ * 
+ * Source     : Leet Code
+ * Difficulty : Dynamic Programming - Medium
+ * Problem    : 
+ * 
+ * Time Complexity  : O(n * n)        
+ * Space complexity : O(n * n) 
+ ********************************************************************************************/
+
+namespace DataStructures.Algorithms.DynamicProgramming
+{
+    internal class PredictWinner : IExecute
+    {
+        public void Execute()
+        {
+            Console.WriteLine("###LeetCode###");
+            Console.WriteLine("486. Predict the Winner");
+            Console.WriteLine("Difficulty Level - MEDIUM\n");
+
+            Console.WriteLine("Using Dynamic Programming\n");
+
+            int[] arr = new int[] { 1, 5, 2 };
+            Console.WriteLine("The Winner is " + IsWinner(arr));
+        }
+
+        public bool IsWinner(int[] nums)
+        {
+            int n = nums.Length;
+            int[,] dp = new int[n, n];
+
+            for (int i = n - 1; i >= 0; --i)
+            {
+                for (int j = i; j < n; ++j)
+                {
+                    if (i == j)
+                    {
+                        dp[i, j] = nums[i];
+                    }
+                    else
+                    {
+                        dp[i, j] = Math.Max(nums[i] - dp[i + 1, j], nums[j] - dp[i, j - 1]);
+                    }
+                }
+            }
+
+            return dp[0, n - 1] >= 0;
+        }
+    }
+}
---
+++ DataStructures.Algorithms/DynamicProgramming/SoupServings.cs
@@ -0,0 +1,70 @@
+﻿/********************************************************************************************
+ * Soup Servings
+ * 
+ * Source     : Leet Code
+ * Difficulty : Dynamic Programming - Medium
+ * Problem    : https://leetcode.com/problems/soup-servings/description/ 
+ * Solution   : 
+ * 
+ * Time Complexity  : O(n * n)        
+ * Space complexity : O(n * n) 
+ ********************************************************************************************/
+
+namespace DataStructures.Algorithms.DynamicProgramming
+{
+    internal class SoupServings : IExecute
+    {
+        Dictionary<Tuple<int, int>, double> memoization = new Dictionary<Tuple<int, int>, double>();
+
+        public void Execute()
+        {
+            Console.WriteLine("###LeetCode###");
+            Console.WriteLine("808. Soup Servings");
+            Console.WriteLine("Difficulty Level - MEDIUM\n");
+
+            Console.WriteLine("Using Dynamic Programming\n");
+
+            Console.WriteLine("The Probability is " + FindProbability(50));
+        }
+
+        public double FindProbability(int num)
+        {
+            if (num > 4451)
+            {
+                return 1.0;
+            }
+
+            num = (num + 24) / 25;
+
+            return dp(num, num);
+        }
+
+        private double dp(int a, int b)
+        {
+            if (a <= 0 && b <= 0)
+            {
+                return 0.5;
+            }
+
+            if (a <= 0)
+            {
+                return 1.0;
+            }
+
+            if (b <= 0)
+            {
+                return 0.0;
+            }
+
+            var key = Tuple.Create(a, b);
+
+            if (memoization.ContainsKey(key))
+            {
+                return memoization[key];
+            }
+
+            memoization[key] = 0.25 * (dp(a - 4, b) + dp(a - 3, b - 1) + dp(a - 2, b - 2) + dp(a - 1, b - 3));
+            return memoization[key];
+        }
+    }
+}
