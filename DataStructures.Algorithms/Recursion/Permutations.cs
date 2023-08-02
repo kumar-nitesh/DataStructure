5f97738c16c9ad27d3b8b1a890e172f9cb95208e
Nitesh Kumar <KumarNitesh@outlook.com>
2023-08-02 22:35:06 +0530

    LeetCode # 46 Permutations (Backtrack)

--- DataStructures.Algorithms/Recursion/Permutations.cs
+++ DataStructures.Algorithms/Recursion/Permutations.cs
@@ -1,52 +1,59 @@
 ﻿/********************************************************************************************
- * Combinations
+ * Permutations
  * 
  * Source     : LeetCode
  * Difficulty : Medium
- * Problem    : https://leetcode.com/problems/combinations/
- * Solution   : https://www.youtube.com/watch?v=7IQHYbmuoVU
+ * Problem    : https://leetcode.com/problems/permutations/
+ * Solution   : https://chadgolden.com/blog/finding-all-the-permutations-of-an-array-in-c-sharp
  * 
- * Time Complexity  : O(2^n)        
- * Space complexity : O(k) 
+ * Time Complexity  : O(n⋅* n!)        
+ * Space complexity : O(n) 
  ********************************************************************************************/
 
 namespace DataStructures.Algorithms.Recursion
 {
-    public class Combinations : IExecute
+    public class Permutations : IExecute
     {
         public void Execute()
         {
             Console.WriteLine("###LeetCode###");
-            Console.WriteLine("77. Combinations");
+            Console.WriteLine("46. Permutations");
             Console.WriteLine("Difficulty Level - MEDIUM");
 
             Console.WriteLine("Using BackTracking");
-            var result = Find(4, 2);
+            var result = Find(new int[] { 1, 2, 3 });
 
             Print(result);
         }
 
-        public IList<IList<int>> Find(int n, int k)
+        public IList<IList<int>> Find(int[] nums)
         {
             var result = new List<IList<int>>();
-            Backtrack(n, k, 1, new List<int>(), result);
+            Backtrack(nums, 0, nums.Length - 1, result);
             return result;
         }
 
-        private void Backtrack(int n, int k, int start, List<int> combination, List<IList<int>> result)
+        private void Backtrack(int[] nums, int start, int end, List<IList<int>> result)
         {
-            if (combination.Count == k)
+            if (start == end)
             {
-                result.Add(new List<int>(combination));
+                result.Add(new List<int>(nums));
                 return;
             }
 
-            for (int i = start; i <= n; i++)
+            for (int i = start; i <= end; i++)
             {
-                combination.Add(i);
-                Backtrack(n, k, i + 1, combination, result);
-                combination.RemoveAt(combination.Count - 1);
+                Swap(ref nums[start], ref nums[i]);
+                Backtrack(nums, start + 1, end, result);
+                Swap(ref nums[start], ref nums[i]);
             }
+        }
+
+        private void Swap(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
         }
 
         private void Print(IList<IList<int>> result)
