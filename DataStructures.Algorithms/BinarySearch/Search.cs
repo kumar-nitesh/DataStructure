6c93280baca935da910a07e089ece220a56fa461
Nitesh Kumar <KumarNitesh@outlook.com>
2023-08-18 15:52:50 +0530

    Search and Sort

---
+++ DataStructures.Algorithms/BinarySearch/Search.cs
@@ -0,0 +1,57 @@
+﻿/********************************************************************************************
+ * Binary Search
+ * 
+ * Source     : LeetCode
+ * Difficulty : Easy
+ * 
+ * Time Complexity  : O(log₂N) // T(n) = n..n/2/..n/4...1 = log₂n     
+ * Space complexity : O(1) 
+ ********************************************************************************************/
+
+namespace DataStructures.Algorithms.BinarySearch
+{
+    public class Search : IExecute
+    {
+        public void Execute()
+        {
+            Console.WriteLine("###LeetCode###");
+            Console.WriteLine("704. Binary Search");
+            Console.WriteLine("Difficulty Level - EASY");
+
+            int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
+
+            Console.WriteLine("The Binary Search index is : " + Algorithm(nums, 5));
+        }
+
+        /// <summary>
+        /// Binary Search(Iterative) - Decrease and Conquer Algorithm
+        /// </summary>
+        public int Algorithm(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2; // To avoid overflow exception
+
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+
+                if (nums[mid] > target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+
+    }
+}
---
+++ DataStructures.Algorithms/BinarySearch/ShipPackage.cs
@@ -0,0 +1,88 @@
+﻿/********************************************************************************************
+ * Minimum Days to Ship
+ * 
+ * Source     : LeetCode
+ * Difficulty : Medium
+ * 
+ * Time Complexity  : O(n) * O(log(max-min))      
+ * Space complexity : O(1) 
+ ********************************************************************************************/
+
+namespace DataStructures.Algorithms.BinarySearch
+{
+    public class ShipPackage : IExecute
+    {
+        public void Execute()
+        {
+            Console.WriteLine("###LeetCode###");
+            Console.WriteLine("1011.  Capacity To Ship Packages Within D Days");
+            Console.WriteLine("Difficulty Level - MEDIUM");
+
+            Console.WriteLine("Using Binary Search\n");
+            int[] sample1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("The Min days to ship is : " + ShipWithinDays(sample1, 5));
+
+            int[] sample2 = new int[] { 3, 2, 2, 4, 1, 4 };
+            Console.WriteLine("The Min days to ship is : " + ShipWithinDays(sample2, 3));
+
+            int[] sample3 = new int[] { 1, 2, 3, 1, 1 };
+            Console.WriteLine("The Min days to ship is : " + ShipWithinDays(sample3, 4));
+        }
+
+        /// <summary>
+        /// Greedy Technique and Binary Search Approach
+        /// </summary>  
+        public int ShipWithinDays(int[] weights, int days)
+        {
+            int minimumWeight = weights[0];
+            int maximumWeight = 0;
+            int currentGuess = 0;
+            int bestGuess = -1;
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                if (weights[i] > minimumWeight)
+                {
+                    minimumWeight = weights[i];
+                }
+
+                maximumWeight += weights[i];
+            }
+
+            while (minimumWeight <= maximumWeight)
+            {
+                currentGuess = (maximumWeight + minimumWeight) / 2; // mid
+                int noOfDays = TotalDaysToShip(weights, currentGuess);
+
+                if (noOfDays <= days)
+                {
+                    bestGuess = currentGuess;
+                    maximumWeight = currentGuess - 1;
+                }
+                else
+                {
+                    minimumWeight = currentGuess + 1;
+                }
+            }
+
+            return bestGuess;
+        }
+
+        private int TotalDaysToShip(int[] weights, int capacity)
+        {
+            int days = 1;
+            int currentShip = 0;
+            foreach (int weight in weights)
+            {
+                if (currentShip + weight > capacity)
+                {
+                    currentShip = 0;
+                    days++;
+                }
+                currentShip += weight;
+            }
+            return days;
+        }
+
+    }
+}
